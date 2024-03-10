7fb70aeb77a19257598fa8afc6c4e22cc9e3eaae
Mario Roberto <mrgomez.dev@gmail.com>
2024-03-09 21:56:37 -0600

    configuration white mode

--- project/web_design/js/script.js
+++ project/web_design/js/script.js
@@ -1,8 +1,28 @@
-<script>
-function enviarWhatsApp() {
-    const telefono = "50377563510";
-    var mensaje = "Hola, vi tu portafolio y quiero comunicarme contigo.";
-    window.open("https://api.whatsapp.com/send?phone=" + telefono + "&text=" + encodeURIComponent(mensaje), "_blank");
-    return false; 
-}
-</script>
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleccionar el botón
+    const button = document.querySelector('.navbar-button');
+
+    // Función para cambiar entre modos
+    function toggleDarkLightMode() {
+        // Cambiar el modo de la página
+        document.body.classList.toggle('light-mode');
+        document.querySelector('header').classList.toggle('light-mode');
+        document.querySelectorAll('.menu-options a').forEach(function(link) {
+            link.classList.toggle('light-mode');
+        });
+        document.querySelectorAll('.menu-options li').forEach(function(li) {
+            li.classList.toggle('light-mode');
+        });
+        document.querySelector('.navbar-button').classList.toggle('light-mode');
+
+        // Cambiar el texto del botón
+        if (button.textContent === 'Black') {
+            button.textContent = 'White';
+        } else {
+            button.textContent = 'Black';
+        }
+    }
+
+    // Agregar un evento de clic al botón
+    button.addEventListener('click', toggleDarkLightMode);
+});
